a3979d96fa63647a389b47d140c02746431d59af
Leo Monti <leomontigatti@gmail.com>
2023-01-12 13:25:19 -0300

    Added DB settings for testing

--- mirana/settings.py
+++ mirana/settings.py
@@ -82,12 +82,24 @@ WSGI_APPLICATION = "mirana.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": config("DB_NAME"),
+            "USER": config("DB_USER"),
+            "PASSWORD": config("DBPASSWORD"),
+            "HOST": "localhost",
+            "PORT": "",
+        }
+    }
 
 
 # Password validation
